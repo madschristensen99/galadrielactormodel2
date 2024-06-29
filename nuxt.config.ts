67c2ea28f4d470e879e2bcc1b58a221833618bbb
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2024-06-28 20:29:44 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,3 +6,11 @@ export default defineNuxtConfig({
     '/': { prerender: true },
   },
 });
+export default {
+  head: {
+    script: [
+      { src: 'https://cdn.ethers.io/lib/ethers-5.2.esm.min.js', type: 'module' }
+    ],
+    __dangerouslyDisableSanitizers: ['script'],
+  },
+}
